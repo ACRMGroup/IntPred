b3c3e47649d2abcfa0bd18addea81e7f714b824b
northeyt <northeyt@gmail.com>
2016-02-10 14:17:38 +0000

    option to supply model file at cmd line

--- bin/testPredictor.pl
+++ bin/testPredictor.pl
@@ -7,10 +7,25 @@ use Main;
 use ConfigReader;
 use Getopt::Long;
 
+my $modelFile;
+
+GetOptions('m=s', \$modelFile);
+
 @ARGV or Usage();
 
 my $configFile = shift @ARGV;
 my $cReader    = ConfigReader->new($configFile);
+
+# Use cmd-line specified model file if available
+if ($modelFile) {
+    if ($cReader->exists('Predictor', 'modelFile')) {
+        $cReader->setval('Predictor', 'modelFile', $modelFile) 
+    }
+    else {
+        $cReader->newval('Predictor', 'modelFile', $modelFile);
+    }
+}
+
 my $predictor  = $cReader->getPredictor();
 
 my $trainData  = $cReader->getTrainingSet();
