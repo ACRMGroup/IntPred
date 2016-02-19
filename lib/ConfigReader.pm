050264b09f84598f4a737f565bf47c54f8c6f916
northeyt <northeyt@gmail.com>
2016-02-19 15:50:58 +0000

    refactor

--- lib/ConfigReader.pm
+++ lib/ConfigReader.pm
@@ -25,10 +25,8 @@ has 'config' => (
 around 'val' => sub {
     my $orig  = shift;
     my $class = shift;
-
     croak "Value for section $_[0], parameter $_[1] is not defined!"
         if ! $class->exists(@_);
-
     return $class->$orig(@_);
 };
 
@@ -36,7 +34,10 @@ around 'BUILDARGS' => sub {
     my $orig  = shift;
     my $class = shift;
 
-    if (@_ == 1) {
+    if (! @_){
+        $class->$orig(config => Config::IniFiles->new());
+    }
+    elsif (@_ == 1) {
         $class->$orig(config => $_[0]);
     }
     else {
@@ -53,7 +54,6 @@ sub getPredictor {
 
 sub createDataSetCreator {
     my $self = shift;
-
     my $creator
         = DataSet::Creator::Master->new(
             model   => $self->createInstanceModel(),
@@ -89,10 +89,8 @@ sub createPropCalc {
 sub addTestSetInputFileAndFormat {
     my $self = shift;
     my ($inFile, $format) = @_;
-    
     # Make sure that input file is an absolute path
     my $inFileAbsPath = File::Spec->rel2abs($inFile);
-    
     $self->newval(qw(TestSet inFile       $inFileAbsPath));
     $self->newval(qw(TestSet inFileFormat $format));
 }
@@ -123,20 +121,21 @@ sub getModelFile {
 
 sub getTrainingSet {
     my $self = shift;
-    return $self->getDataSetOfType("TrainingSet");
+    my $arff = $self->getArffForSet("TrainingSet");
+    return DataSet->new(arff => $arff, lazyLoadArff => 1);
 }
 
 sub getTestSet {
     my $self = shift;
-    return $self->getDataSetOfType("TestSet");
+    my $arff = $self->getArffForSet("TestSet");
+    return DataSet->new(arff =>$arff);
 }
 
-sub getDataSetOfType {
+sub getArffForSet {
     my $self = shift;
     my $type = shift;
     my $arffFile = $self->_getPathForVal($type, "ARFF");
-    my $arff     = ARFF::FileParser->new(file => $arffFile)->parse();
-    return DataSet->new($arff);
+    return ARFF::FileParser->new(file => $arffFile)->parse();
 }
 
 sub getTrainingSetARFF {
@@ -154,12 +153,14 @@ sub _getPathForVal {
     my $self = shift;
     my ($section, $parameter)  = @_;
     my $value = $self->val($section, $parameter);
-    return File::Spec->file_name_is_absolute($value) ? $value
+    return File::Spec->file_name_is_absolute($value) || -l $value ? $value
         : $self->_getConfigDir() . "/" . $value;
 }
 
 sub _getConfigDir {
     my $self = shift;
+    croak "config has no file name! Was a config file supplied?"
+        if ! $self->config->GetFileName;
     my ($fName, $configDir, $suffix) = fileparse($self->config->GetFileName);
     return $configDir;
 }
@@ -167,15 +168,11 @@ sub _getConfigDir {
 sub readLabelsFile {
     my $self       = shift;
     my $labelsFile = $self->_getPathForVal(qw(TestSet labelsFile)); 
-    
     open(my $IN, "<", $labelsFile) or die "Cannot open file $labelsFile, $!";
-
     my %pID2label = ();
-    
     while (my $line = <$IN>) {
         chomp $line;
         # example line = 2wap:A:102:I
-
         my ($patchID, $label) = $line =~ /(.*):(.*)/g; # matches last :
         $pID2label{$patchID} = $label;
     }
@@ -185,9 +182,7 @@ sub readLabelsFile {
 sub readPatchDir {
     my $self     = shift;
     my $patchDir = $self->_getPathForVal(qw(TestSet patchDir));
-    
     my %pdbID2pSummaries = ();
-    
     opendir(my $DIR, $patchDir) or die "Cannot open patch dir $patchDir, $!";
     while (my $fName = readdir($DIR)) {
         next if $fName =~ /^\./; # Skip . and ..
