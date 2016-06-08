f39468d817372e6a4582394ca289ce33e0301567
northeyt <northeyt@gmail.com>
2016-06-08 12:19:56 +0100

    config file can be specified at cmd line

--- bin/standardize-test-arff.pl
+++ bin/standardize-test-arff.pl
@@ -3,11 +3,16 @@ use strict;
 use warnings;
 use lib ("/home/bsm3/zcbtfo4/scripts/lib/IntPred/lib");
 use ConfigReader;
+use Getopt::Long;
+
+my $configFile;
+
+GetOptions("c=s" => \$configFile);
 
 @ARGV or die "Please supply an unstandardized test set .arff";
 my $testArff = shift @ARGV;
 
-my $config = ConfigReader->new("IntPred.config.ini");
+my $config = ConfigReader->new(defined $configFile ? $configFile : "IntPred.config.ini");
 $config->exists("TestSet", "ARFF") ? $config->setval("TestSet", "ARFF", $testArff)
     : $config->newval("TestSet", "ARFF", $testArff);
 
