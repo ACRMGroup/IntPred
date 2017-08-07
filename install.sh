e823884f6af530771e0c7563c1317fa6a5485df9
AndrewCRMartin <andrew@bioinf.org.uk>
2017-08-07 12:21:23 +0100

    TCLlib not IntPred\!

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+H=`pwd`
+
+# System preliminaries
 sudo yum -y install perl-App-cpanminus expat
 sudo perl -MCPAN -e shell <<EOF
 install CPAN
@@ -8,3 +11,7 @@ o conf commit
 EOF
 sudo cpanm Bio::Perl
 
+# TCNlib
+cd packages
+unzip TCNlib-master.zip
+cd TCNlib-master
