4e7fbc5c77316bffe48c173710b241a66253b308
AndrewCRMartin <andrew@bioinf.org.uk>
2017-08-07 13:27:29 +0100

    Bug fixes in TCNlib-master.zip

--- install.sh
+++ install.sh
@@ -3,16 +3,17 @@ H=`pwd`
 
 # System preliminaries
 if [ 'x' == 'y' ]; then
-   sudo yum -y install perl-App-cpanminus expat
-   sudo perl -MCPAN -e shell <<EOF
-   install CPAN
-   reload cpan
-   install Module::Build
-   o conf prefer_installer MB
-   o conf commit
-   EOF
-   sudo cpanm Bio::Perl
+sudo yum -y install perl-App-cpanminus expat
+sudo perl -MCPAN -e shell <<EOF
+install CPAN
+reload cpan
+install Module::Build
+o conf prefer_installer MB
+o conf commit
+EOF
+sudo cpanm Bio::Perl
 fi
+
 
 # TCNlib
 cd packages
@@ -20,8 +21,9 @@ unzip TCNlib-master.zip
 cd TCNlib-master
 
 # Temp fix until Tom has fixed it!a
-sed 's/www.bioinf.org.uk\/intpred\/packages / www.bioinf.org.uk\/intpred\/src\/packages/' getexternalpackages > foo
+sed 's/www.bioinf.org.uk\/intpred\/packages/www.bioinf.org.uk\/intpred\/src\/packages/' getexternalpackages > foo
 mv foo getexternalpackages
+chmod +x getexternalpackages
 
 ./setup.pl
 ./runtests.pl
