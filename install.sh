8ea243d85a7e289d1e6954a3380455efcb0dd006
AndrewCRMartin <andrew@bioinf.org.uk>
2017-08-07 12:52:26 +0100

    Temp fix in TCNlib install

--- install.sh
+++ install.sh
@@ -1,17 +1,29 @@
+### NOTE! You must run this as root OR have sudo permissions ###
 H=`pwd`
 
 # System preliminaries
-sudo yum -y install perl-App-cpanminus expat
-sudo perl -MCPAN -e shell <<EOF
-install CPAN
-reload cpan
-install Module::Build
-o conf prefer_installer MB
-o conf commit
-EOF
-sudo cpanm Bio::Perl
+if [ 'x' == 'y' ]; then
+   sudo yum -y install perl-App-cpanminus expat
+   sudo perl -MCPAN -e shell <<EOF
+   install CPAN
+   reload cpan
+   install Module::Build
+   o conf prefer_installer MB
+   o conf commit
+   EOF
+   sudo cpanm Bio::Perl
+fi
 
 # TCNlib
 cd packages
 unzip TCNlib-master.zip
 cd TCNlib-master
+
+# Temp fix until Tom has fixed it!a
+sed 's/www.bioinf.org.uk\/intpred\/packages / www.bioinf.org.uk\/intpred\/src\/packages/' getexternalpackages > foo
+mv foo getexternalpackages
+
+./setup.pl
+./runtests.pl
+# This needs PyMol
+
