9d6b39ecfab8bc4ab437bb8b4b3f4bee0eebd55c
Tom Northey <northeyt@gmail.com>
2017-12-09 14:08:23 +0000

    Add IntPred:Epi functionality

--- bin/runIntPred.pl
+++ bin/runIntPred.pl
@@ -8,17 +8,36 @@ use ConfigReader;
 use Getopt::Long;
 use Carp;
 
-my $inFileFormat   = "pdb";
+my $inFileFormat = "pdb";
 my $help = 0;
+my $predOutputFile;
+my $patchOutDir;
+my $outputAsCSV = 0;
+my $runIntPredEpi = 0;
 
 GetOptions("f=s" => \$inFileFormat,
+           "o=s" => \$predOutputFile,
+           "p=s" => \$patchOutDir,
+           "c"   => \$outputAsCSV,
+           "e"   => \$runIntPredEpi,
            "h"   => \$help);
+
 Usage() and exit(0) if $help;
 print "You must supply an input file!" and Usage() and exit(1)
     if ! @ARGV;
 
+my $OUT;
+
+if ($predOutputFile) {
+    open($OUT, ">", $predOutputFile)
+        or die "Cannot open output file $predOutputFile, $!";
+}
+else {
+    $OUT = *STDOUT;
+}
+
 my $inFile = $ARGV[0];
-my $rConfig   = _getConfigReader();
+my $rConfig   = _getConfigReader($runIntPredEpi);
 $rConfig->addTestSetInputFileAndFormat($inFile, $inFileFormat);
 
 my $predictor = $rConfig->getPredictor();
@@ -27,24 +46,49 @@ $predictor->testSet($testData);
 $predictor->runPredictor();
 $predictor->assignPredictionScoresToTestSet();
 
-print map {$_->id . ": " . $_->predScore . "\n"} @{$testData->instancesAref()};
+if ($outputAsCSV) {
+    $predictor->outputParser->printCSVString($OUT);
+}
+else {
+    print {$OUT} map {$_->id . ": " . $_->predScore . "\n"} @{$testData->instancesAref()};
+}
+
+if ($patchOutDir) {
+    print "Writing patch files to dir $patchOutDir\n";
+    my $groupByChain = 1;
+    $testData->writePatchFilesToDir($patchOutDir, $groupByChain);
+}
 
 print "Finished!\n";
 
 sub _getConfigReader {
-    my $configFile     = "$FindBin::Bin/../config/runIntPred.ini";
+    my $runIntPredEpi = shift @_;
+    my $configFile    = $runIntPredEpi ? "$FindBin::Bin/../config/runIntPredEpi.ini"
+        : "$FindBin::Bin/../config/runIntPred.ini";
     return ConfigReader->new($configFile);
 }
 
 sub Usage {
     print <<EOF;
-$0 [-f pdb|pqs|file] input_file
+$0 [-f pdb|pqs|file] [-p patch_out_dir] [-o pred_out_file] [-c] [-e] input_file
 
 opts
 
  -f : Format of the input file. Default = pdb. This sets what IntPred
       expects the identifiers of the input file to be: pdb code, pqs codes
       or file names.
+
+ -o : Output file for prediction scores to be written to. If not supplied,
+      scores will be written to stdout.
+
+ -p : Directory to save patch files to. Patch files can be used to map
+      patches to constituent residues.
+
+ -c : Set output predictions to have WEKA CSV format. Use this for
+      compatibility with transformPatch2ResiduePred.pl
+
+ -e : run IntPred:Epi for epitope prediction.
+
 args
 
  input_file : input file following the IntPred input format (see below).
