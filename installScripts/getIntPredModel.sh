cc5e8fb9d896e1040af709b73df78d6d069d6f89
AndrewCRMartin <andrew@bioinf.org.uk>
2020-02-14 17:38:21 +0000

    Updates to TCNLib

--- installScripts/getIntPredModel.sh
+++ installScripts/getIntPredModel.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd data/IntPred/
-wget "http://bioinf.org.uk/intpred/src/models/IntPred.model.gz" && gzip -d IntPred.model.gz
+wget "http://bioinf.org.uk/intpredsrc/models/IntPred.model.gz" && gzip -d IntPred.model.gz
